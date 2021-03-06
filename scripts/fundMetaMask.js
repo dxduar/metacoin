4c3eaca24dae71877d364d8a74881d8f6cda8099
dxduar <79992809+dxduar@users.noreply.github.com>
2021-03-07 00:55:55 +0700

    Update fundMetaMask.js

--- scripts/fundMetaMask.js
+++ scripts/fundMetaMask.js
@@ -3,4 +3,4 @@
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
-web3.eth.sendTransaction({ from: web3.eth.accounts[0], to: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81', value: 1e19 })
+web3.eth.sendTransaction({ from: web3.eth.accounts[0], to: '0xFdb044f5197D43e26b91A303481Ab463A103BbC4', value: 1e19 })

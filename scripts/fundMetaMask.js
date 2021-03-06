55a88d60e135413b5b399188f9c003f8cad44de4
dxduar <79992809+dxduar@users.noreply.github.com>
2021-03-07 01:08:19 +0700

    Update fundMetaMask.js

--- scripts/fundMetaMask.js
+++ scripts/fundMetaMask.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/f19c5e0f2fd047e9bc14b5fdd5577e5b'))
 
 web3.eth.sendTransaction({ from: web3.eth.accounts[0], to: '0xFdb044f5197D43e26b91A303481Ab463A103BbC4', value: 1e19 })

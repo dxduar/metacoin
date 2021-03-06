aa11cbd5129cac79779cf57f7af48d8cbbf52401
dxduar <79992809+dxduar@users.noreply.github.com>
2021-03-07 00:29:20 +0700

    Add files via upload

---
+++ app/scripts/networks.js
@@ -0,0 +1,32 @@
+module.exports = {
+  networks: {
+    1: {
+      addressUrl: 'https://etherscan.io/address/',
+      txUrl: 'https://etherscan.io/tx/',
+      metacoin: '0x4c6ec08cf3fc987c6c4beb03184d335a2dfc4042',
+      relayHub: '0xB1E47968aD4909b9eb693c212feA22D0419D2D56',
+      paymaster: '0x8fe9bdc125f26bf50f969cb32c4dcf3a353ea02b',
+      forwarder: '0xa530F85085C6FE2f866E7FdB716849714a89f4CD'
+    },
+    3: {
+      addressUrl: 'https://dashboard.tenderly.co/contract/ropsten/',
+      txUrl: 'https://dashboard.tenderly.co/tx/ropsten/',
+      // addressUrl: 'https://ropsten.etherscan.io/address/',
+      // txUrl: 'https://ropsten.etherscan.io/tx/',
+      metacoin: '0x49139BdC9d484068851d2862ef8233c4D31e679b',
+      relayHub: '0x29e41C2b329fF4921d8AC654CEc909a0B575df20',
+      paymaster: '0x8057c0fb7089BB646f824fF4A4f5a18A8d978ecC',
+      forwarder: '0x25CEd1955423BA34332Ec1B60154967750a0297D'
+    },
+    4: {
+      addressUrl: 'https://dashboard.tenderly.co/contract/rinkeby/',
+      txUrl: 'https://dashboard.tenderly.co/tx/rinkeby/',
+      // addressUrl: 'https://rinkeby.etherscan.io/address/',
+      // txUrl: 'https://rinkeby.etherscan.io/tx/',
+      metacoin: '0xeEaAF1746224399CecE52228831B6CaCea18769D',
+      relayHub: '0x53C88539C65E0350408a2294C4A85eB3d8ce8789',
+      paymaster: '0x43d66E6Dce20264F6511A0e8EEa3f570980341a2',
+      forwarder: '0x956868751Cc565507B3B58E53a6f9f41B56bed74'
+    }
+  }
+}
